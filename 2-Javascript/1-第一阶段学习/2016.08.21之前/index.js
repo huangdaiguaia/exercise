cf5dfb173609eaf581e432541fcc874db4775fdf
huangdaiguaia <2110324679@qq.com>
2017-05-28 14:33:34 +0800

    20170528

---
+++ 2-Javascript/1-第一阶段学习/2016.08.21之前/index.js
@@ -0,0 +1,8 @@
+//找到小狗图片
+dogobj=document.getElementById('dogid');
+//给小狗图片加事件
+dogobj.onclick=function(){
+	//代码段
+	alert(1);
+}
+//触发事件
---
+++ 3-JQuery/2016.09.02/index.js
@@ -0,0 +1,4 @@
+$(function(){
+	//当DOM加载完毕时执行以下所有代码
+	$('body').css({'background':'#00f'});
+});
